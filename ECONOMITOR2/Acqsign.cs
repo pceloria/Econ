2927e76f7ef10a1b1b2615bb8f68dc8f03c7b606
mtajeria <mati_taje@hotmail.com>
2016-07-02 19:05:12 -0300

    data_y_acq
    agregamos modulo de data y modulo de acq con funciones y variables
    tentativas.besoooooooo

---
+++ ECONOMITOR2/Acqsign.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ECONOMITOR2
+{
+
+
+    //static const int ECG_WAVE = 1;
+    //static const int ECG_PARAM = 2;
+    //static const int NIBP_PARAM = 3;
+    //static const int SPO2_PARAM = 4;
+    //static const int TEMP = 5;
+    //static const int SOFT_VERSION = 252;
+    //static const int HARD_VERSION = 253;
+    //static const int SPO2_WAVE = 254;
+    //static const int RESP_WAVE = 255;
+
+    //static const int ECG_WAVEfreq = 250;
+    //static const int ECG_PARAMfreq = 1;
+    //static const int NIBP_PARAMfreq = 2;
+    //static const int SPO2_PARAMfreq = 1;
+    //static const int TEMPfreq = 1;
+    //static const int SPO2_WAVEfreq = 50;
+    //static const int RESP_WAVEfreq = 50;
+
+
+    public class Acqsign
+    {
+        int tipo_de_variable;
+        int valor[];
+        static int varAd = 1;
+
+        private static void aq(){
+            
+        switch(varAd){
+            case 1:
+                Data.updateTemperatura(100);
+                break;
+            case 2:
+                addECGdata(valor);
+                if (full)
+                    Data.updateECG();
+                break;
+            default:
+                break;
+
+
+        }
+        }
+    }
+
+
+}
