feb53eeb087744750b0f3a18de5f23dfb2c6d039
ipastore <ignaciopastorebenaim@gmail.com>
2016-07-02 23:27:18 -0300

    Cambie data

--- ECONOMITOR2/Data.cs
+++ ECONOMITOR2/Data.cs
@@ -14,31 +14,47 @@ namespace ECONOMITOR2
             ECG = new int[5000];
             lastPositionInECGArray = 0;
         }
-       
+
         private static int lastPositionInECGArray;
 
         private static int Temperatura;
         private static int[] ECG;
 
 
-        public static void updateTemperatura(int Temperatura){
+        public static void updateTemperatura(int Temperatura)
+        {
             Data.Temperatura = Temperatura;
-           // drawTemperatura();
+            drawTemperatura();
+
         }
-        public static void updateECG(int[] newData)
+
+
+        public static int[] updateECG(int[] newData)
         {
             // hacer un for a partir del indice j (guardar ese dato) y actualizar con newData
-           // Data.ECG[lastPositionInECGArray: (lastPositionInECGArray + newData.Length)] = newData;
+            // Data.ECG[lastPositionInECGArray: (lastPositionInECGArray + newData.Length)] = newData;
             // chequear que no se haya llegado al final del array
 
-            drawECG();
+            for (int i = 0; i < newData.Length; i++)
+            {
+                Data.ECG[i + lastPositionInECGArray] = newData[i];
+            }
+
+            lastPositionInECGArray = lastPositionInECGArray + newData.Length;
+
+            if (lastPositionInECGArray + newData.Length > 5000)
+
+                lastPositionInECGArray = 0;
+
+            return ECG;
+
         }
 
         private static void drawTemperatura()
         {
             // llamar a funcion de Nacho y Pau que actualice en pantalla el valor de temperatura
         }
-        private static void drawECG()
+        private static void drawECG(int[] dataECG)
         {
             // llamar a funcion de Nacho y Pau que actualice en pantalla el plot de ECG
         }
